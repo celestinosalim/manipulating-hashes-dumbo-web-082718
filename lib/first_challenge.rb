dbaa79aea6f83333746a7cc9a432eeaf574178d8
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-23 23:42:42 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -23,7 +23,7 @@ contacts.each do | person, data |
     data.each do | attribute, value |
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
-          [].delete_if{|x| x == "strawberry"}
+          [flavor].delete_if{|x| x == "strawberry"}
         end
       end
     end
